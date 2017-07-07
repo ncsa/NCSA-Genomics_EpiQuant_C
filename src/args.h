289f6bd44a85904593fe92209f89ccc183703c6b
rchui <ryan.w.chui@gmail.com>
2017-07-07 16:14:51 -0500

    Adding comments

--- src/args.h
+++ src/args.h
@@ -1,6 +1,14 @@
 #ifndef ARGS_H
 #define ARGS_H
 
-void getArgs(int argc, char *argv[], int *pTrans, int *sTrans);
+// Gets and sets user given options.
+// Params:
+// 		argc (int) number of user arguments.
+// 		argv (char *[]) user arguments.
+// 		pTrans (int *) option to tranpose phenotype file.
+// 		sTrans (int *) option to tranpose snp file.
+// 		pDelim (char *) phenotype file delimiter.
+// 		sDelim (char *) snp file delimiter.
+void getArgs(int argc, char *argv[], int *pTrans, int *sTrans, char *pDelim, char *sDelim);
 
 #endif
