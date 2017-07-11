1b6ef35256df683fe5d8af3e2090ba056cae782c
rchui <ryan.w.chui@gmail.com>
2017-07-11 14:40:49 -0500

    Added comments

--- src/parser.h
+++ src/parser.h
@@ -1,7 +1,20 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-void getPhenotype();
-void getSNP();
+// Gets phenotype data from phenotype file.
+// Params:
+//      pTrans (int64_t) tranpose phenotype data.
+//      pDelim (int64_t) phenotype data delimiter.
+//      pCol (int64_t) phenotype data columns to ignore.
+//      phenoFile (char *) phenotype file name.
+void getPhenotype(int64_t pTrans, char pDelim, in64_t pCol, char *phenoFile);
+
+// Gets snp data from snp file.
+// Params:
+//      sTrans (int64_t) tranpose snp data.
+//      sDelim (int64_t) snp data delimiter.
+//      sCol (int64_t) snp data columns to ignore.
+//      snpFile (char *) snp file name.
+void getSNP(int64_t sTrans, char sDelim, int64_t sCol, char *snpFile);
 
 #endif
