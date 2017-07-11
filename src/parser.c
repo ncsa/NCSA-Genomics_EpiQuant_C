539f706646640b2611aba5b50f91b89de9495de9
rchui <ryan.w.chui@gmail.com>
2017-07-11 14:48:26 -0500

    fixed pCol and sCol to pointers

--- src/parser.c
+++ src/parser.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+
 // Gets phenotype data from phenotype file.
 // Params:
 //      pTrans (int64_t) tranpose phenotype data.
 //      pDelim (int64_t) phenotype data delimiter.
-//      pCol (int64_t) phenotype data columns to ignore.
+//      pCol (int64_t *) phenotype data columns to ignore.
 //      phenoFile (char *) phenotype file name.
-void getPhenotype(int64_t pTrans, char pDelim, in64_t pCol, char *phenoFile) {
-
+void getPhenotype(int64_t pTrans, char pDelim, int64_t *pCol, char *phenoFile) {
+    printf("getPhenotype()\n");
 }
 
 // Gets snp data from snp file.
 // Params:
 //      sTrans (int64_t) tranpose snp data.
 //      sDelim (int64_t) snp data delimiter.
-//      sCol (int64_t) snp data columns to ignore.
+//      sCol (int64_t *) snp data columns to ignore.
 //      snpFile (char *) snp file name.
-void getSNP(int64_t sTrans, char sDelim, int64_t sCol, char *snpFile) {
-
+void getSNP(int64_t sTrans, char sDelim, int64_t *sCol, char *snpFile) {
+    printf("getSNP()\n");
 }
