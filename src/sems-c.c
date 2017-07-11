8b2400580c1d83a8323d7eb5f2cca757ec2d3c54
rchui <ryan.w.chui@gmail.com>
2017-07-11 13:59:19 -0500

    swapped int for int64_t and added column pointers

--- src/sems-c.c
+++ src/sems-c.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include "args.h"
 // #include "omp.h"
 // #include "mpi.h"
 
 int main(int argc, char *argv[]) {
-	int pTrans = 0;
-	int sTrans = 0;
+	int64_t pTrans = 0;
+	int64_t sTrans = 0;
 	char pDelim, sDelim;
+	int64_t *pCol;
+	int64_t *sCol;
 
-	getArgs(argc, argv, &pTrans, &sTrans, &pDelim, &sDelim);
-	printf("%d:%d\n", pTrans, sTrans);
+	getArgs(argc, argv, &pTrans, &sTrans, &pDelim, &sDelim, &pCol, &sCol);
+	printf("%lld:%lld\n", pTrans, sTrans);
 	printf("%c:%c\n", pDelim, sDelim);
 
-	char * snpFile = argv[argc - 1];
-	char * phenoFile = argv[argc - 2];
+	// char * snpFile = argv[argc - 1];
+	// char * phenoFile = argv[argc - 2];
 
+	free(pCol);
+	free(sCol);
 	return 0;
 }
