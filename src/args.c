4129bfd4d647a7c6ca1e2c305d98b4089164bbca
rchui <ryan.w.chui@gmail.com>
2017-07-12 14:20:09 -0500

    Commented out custom try catch

--- src/args.c
+++ src/args.c
@@ -5,10 +5,10 @@
 #include <setjmp.h>
 #include "args.h"
 
-#define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
-#define CATCH } else {
-#define ETRY } }while(0)
-#define THROW longjmp(ex_buf__, 1)
+// #define TRY do{ jmp_buf ex_buf__; if( !setjmp(ex_buf__) ){
+// #define CATCH } else {
+// #define ETRY } }while(0)
+// #define THROW longjmp(ex_buf__, 1)
 
 // Prints help text.
 void printHelp() {
