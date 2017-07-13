8d75d26e14ee7a9f250c5f82cd548676dd7b0d8a
Ryan Chui <Ryan@wirelessprv-10-192-194-67.near.illinois.edu>
2017-07-13 11:59:27 -0500

    Commented printUValues() and getArgs().

--- src/args.h
+++ src/args.h
@@ -9,8 +9,10 @@
 // 		sTrans (int64_t *) option to tranpose snp file.
 // 		pDelim (char *) phenotype file delimiter.
 // 		sDelim (char *) snp file delimiter.
-//      pCol (int64_t **) phenotype columns to ignore.
-//      sCol (int64_t **) snp columns to ignore.
+// 		pCol (int64_t **) phenotype columns to ignore.
+// 		sCol (int64_t **) snp columns to ignore.
+// 		alpha (double *) threshold value.
+// 		outFile (char **) output results file name.
 void getArgs(int64_t argc,
             char *argv[], 
             int64_t *pTrans, 
@@ -19,11 +21,22 @@ void getArgs(int64_t argc,
             char *sDelim, 
             int64_t **pCol, 
             int64_t **sCol, 
-            double *alpha,
+            double *alpha1,
+            double *alpha2, 
             char **outFile);
 
+// Gets and sets user given options.
+// Params:
+// 		pTrans (int64_t *) option to tranpose phenotype file.
+// 		sTrans (int64_t *) option to tranpose snp file.
+// 		pDelim (char *) phenotype file delimiter.
+// 		sDelim (char *) snp file delimiter.
+// 		pCol (int64_t **) phenotype columns to ignore.
+// 		sCol (int64_t **) snp columns to ignore.
+// 		alpha (double *) threshold value.
+// 		outFile (char **) output results file name.
 void printUValues(int64_t pTrans, int64_t sTrans, char pDelim, char sDelim, int64_t *pCol, 
-                  int64_t *sCol, char *phenoFile, char *snpFile, double alpha,
+                  int64_t *sCol, char *phenoFile, char *snpFile, double alpha1, double alpha2, 
                   char *outFile);
 
 #endif
