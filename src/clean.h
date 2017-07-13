b62ea9539c01fa2c7f61169a61d5f777fe462c16
Ryan Chui <Ryan@wirelessprv-10-192-194-67.near.illinois.edu>
2017-07-13 12:04:01 -0500

    Commented

--- src/clean.h
+++ src/clean.h
@@ -1,6 +1,9 @@
 #ifndef CLEAN_H
 #define CLEAN_H
 
+// Frees all allocated data from data structures.
+// Params:
+//      data (struct Data *) phenotype and snp data.
 void clean(struct Data *data);
 
 #endif
